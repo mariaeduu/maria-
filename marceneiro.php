cdd6d59e46004a484dafa56ef434d2d081665a9d
mariaeduu <48128044+mariaeduu@users.noreply.github.com>
2019-03-01 11:57:32 -0300

    Add files via upload

---
+++ marceneiro.php
@@ -0,0 +1,28 @@
+<?php
+
+$medida_tabua1= 300;
+$medida_tabua2=400;
+$medida_tabua3=500;
+
+$pedacos_tabua1= (int) ($medida_tabua1/45);
+$desperdicio_tabua1= (int) ($medida_tabua1%45);
+
+print "foi possível cortar $pedacos_tabua1 da tabua em 3m e sobraram $desperdicio_tabua1 cm de tabua";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
